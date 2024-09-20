1c75a0d76e5f286c14cbbc165b51184aa8d1c615
ardeliaa2303@gmail.com <ardeliaa2303@gmail.com>
2024-09-20 16:57:10 +0700

    part 1

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Nerko One': ['Nerko One','cursive'],
+        'del': ['New Amsterdam','sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
